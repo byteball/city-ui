f2db8ef5a922217eeb598de0b9bf0de39daea1c1
Taump <taump@yandex.ru>
2025-01-30 18:18:57 +0400

    feat: add tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
 import { useRef } from "react";
-import { IRefPhaserGame, PhaserGame } from "./game/PhaserGame";
+import { IRefPhaserGame } from "./game/PhaserGame";
 
 function App() {
     const phaserRef = useRef<IRefPhaserGame | null>(null);
 
     return (
         <div id="app">
-            <PhaserGame ref={phaserRef} />
+            <div className="text-3xl font-bold text-green-500 underline">test</div>
+            {/* <PhaserGame ref={phaserRef} /> */}
         </div>
     );
 }
--- vite/config.prod.mjs
+++ vite/config.prod.mjs
@@ -1,49 +1,58 @@
 import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react';
-
+import path from 'path';
+import { fileURLToPath } from 'url';
 import { defineConfig } from 'vite';
 
-const phasermsg = () => {
-    return {
-        name: 'phasermsg',
-        buildStart() {
-            process.stdout.write(`Building for production...\n`);
-        },
-        buildEnd() {
-            const line = "---------------------------------------------------------";
-            const msg = `Project assembly completed`;
-            process.stdout.write(`${line}\n${msg}\n${line}\n`);
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
-            process.stdout.write(`✨ Done ✨\n`);
-        }
+const phasermsg = () => {
+  return {
+    name: 'phasermsg',
+    buildStart() {
+      process.stdout.write(`Building for production...\n`);
+    },
+    buildEnd() {
+      const line = "---------------------------------------------------------";
+      const msg = `Project assembly completed`;
+      process.stdout.write(`${line}\n${msg}\n${line}\n`);
+
+      process.stdout.write(`✨ Done ✨\n`);
     }
+  }
 }
 
 export default defineConfig({
-    base: './',
-    plugins: [
-        react(),
-        phasermsg(),
-        tailwindcss()
-    ],
-    logLevel: 'warning',
-    build: {
-        rollupOptions: {
-            output: {
-                manualChunks: {
-                    phaser: ['phaser']
-                }
-            }
-        },
-        minify: 'terser',
-        terserOptions: {
-            compress: {
-                passes: 2
-            },
-            mangle: true,
-            format: {
-                comments: false
-            }
+  base: './',
+  plugins: [
+    react(),
+    phasermsg(),
+    tailwindcss()
+  ],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "../src"),
+    },
+  },
+  logLevel: 'warning',
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          phaser: ['phaser']
         }
+      }
+    },
+    minify: 'terser',
+    terserOptions: {
+      compress: {
+        passes: 2
+      },
+      mangle: true,
+      format: {
+        comments: false
+      }
     }
+  }
 });
