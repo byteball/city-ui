c69b8fd838311e4e18d1c3df2c3ea512707faa33
Taump <taump@yandex.ru>
2025-02-03 18:44:41 +0400

    feat: integrate PhaserGame component and update app layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,12 @@
 import { useRef } from "react";
-import { IRefPhaserGame } from "./game/PhaserGame";
+import { IRefPhaserGame, PhaserGame } from "./game/PhaserGame";
 
 function App() {
     const phaserRef = useRef<IRefPhaserGame | null>(null);
 
     return (
-        <div id="app">
-            <div className="text-3xl font-bold text-green-500 underline">test</div>
-            {/* <PhaserGame ref={phaserRef} /> */}
+        <div id="app" className="w-[600px] h-[600px]">
+            <PhaserGame ref={phaserRef} />
         </div>
     );
 }
