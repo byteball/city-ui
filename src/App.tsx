d1642c0840b5de02902c24c69859cc9c58ee80cd
Taump <taump@yandex.ru>
2025-02-03 23:12:46 +0400

    style: format code and add border to App component

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,13 @@ import { useRef } from "react";
 import { IRefPhaserGame, PhaserGame } from "./game/PhaserGame";
 
 function App() {
-    const phaserRef = useRef<IRefPhaserGame | null>(null);
+  const phaserRef = useRef<IRefPhaserGame | null>(null);
 
-    return (
-        <div id="app" className="w-[600px] h-[600px]">
-            <PhaserGame ref={phaserRef} />
-        </div>
-    );
+  return (
+    <div id="app" className="w-[600px] h-[600px] border ">
+      <PhaserGame ref={phaserRef} />
+    </div>
+  );
 }
 
 export default App;
