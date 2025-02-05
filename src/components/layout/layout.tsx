38ab18d45c92cb8c630a7f98e87abe446a2c955a
Taump <taump@yandex.ru>
2025-02-05 23:37:26 +0400

    feat: refactor Layout component structure for improved readability

--- src/components/layout/layout.tsx
+++ src/components/layout/layout.tsx
@@ -8,16 +8,17 @@ interface ILayoutProps {
   children?: React.ReactNode;
 }
 
-export const Layout: FC<ILayoutProps> = () => (<div className="min-w-full min-h-full bg-background-dark">
-  <div className="mb-8">
-    <Header />
-  </div>
+export const Layout: FC<ILayoutProps> = () => (
+  <div className="min-w-full min-h-full bg-background-dark">
+    <div className="mb-8">
+      <Header />
+    </div>
 
-  <div className="container mx-auto sm:px-6 lg:px-8">
-    <Outlet />
-  </div>
+    <div className="container mx-auto sm:px-6 lg:px-8">
+      <Outlet />
+    </div>
 
-  <Footer />
-</div>)
-};
+    <Footer />
+  </div>
+);
 
