86b9567bc6c8a183d080f4e44c006c7e4500fb32
Taump <taump@yandex.ru>
2025-02-12 18:57:49 +0400

    feat: remove setSelectedPlot call from bootstrap function

--- src/bootstrap.ts
+++ src/bootstrap.ts
@@ -1,10 +1,9 @@
 import { initializeStore } from "./store/aa-store";
-import { initializeSettings, setSelectedPlot } from "./store/settings-store";
+import { initializeSettings } from "./store/settings-store";
 
 export const bootstrap = async () => {
   await initializeStore();
   await initializeSettings();
-  setSelectedPlot();
   console.log("log: bootstrap done");
 };
 
