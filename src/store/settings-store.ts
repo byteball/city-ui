0260272665f8c09c9c2d1fb96f2b2b335bb633b1
Taump <taump@yandex.ru>
2025-02-12 19:46:53 +0400

    feat: update settings store to manage selected map unit and add coordinate update functionality

--- src/store/settings-store.ts
+++ src/store/settings-store.ts
@@ -1,6 +1,10 @@
-import { ICoordinates, NonNegativeNumber } from "@/global";
 import { create, StateCreator } from "zustand";
 import { devtools, persist } from "zustand/middleware";
+
+import { ICoordinates, NonNegativeNumber } from "@/global";
+import { toast } from "@/hooks/use-toast";
+import { useAaStore } from "./aa-store";
+import { mapUnitsSelector } from "./selectors/mapUnitsSelector";
 
 const LOCAL_STORAGE_KEY = "settings-store";
 const STORAGE_VERSION = 2; // change this to invalidate old persisted data
@@ -9,8 +13,8 @@ interface SettingsState {
   firstInit: () => void;
   inited: boolean;
 
-  selectedPlot?: { x: NonNegativeNumber; y: NonNegativeNumber };
-  setSelectedPlot: (coordinates?: ICoordinates | null) => void;
+  selectedMapUnit?: { x: NonNegativeNumber; y: NonNegativeNumber };
+  setSelectedMapUnit: (coordinates?: ICoordinates | null) => void;
 }
 
 const storeCreator: StateCreator<SettingsState> = (set, get) => ({
@@ -22,10 +26,10 @@ const storeCreator: StateCreator<SettingsState> = (set, get) => ({
     console.log("log: initialized settings store");
   },
   inited: false,
-  selectedPlot: undefined,
-  setSelectedPlot: (coordinates) => {
-    if (!coordinates) return set({ selectedPlot: undefined });
-    set({ selectedPlot: { x: coordinates.x, y: coordinates.y } });
+  selectedMapUnit: undefined,
+  setSelectedMapUnit: (coordinates) => {
+    if (!coordinates) return set({ selectedMapUnit: undefined });
+    set({ selectedMapUnit: { x: coordinates.x, y: coordinates.y } });
   },
 });
 
@@ -52,5 +56,36 @@ export const useSettingsStore = create<SettingsState>()(
 
 export const initializeSettings = (): void => useSettingsStore.getState().firstInit();
 
-export const setSelectedPlot = (coordinates?: ICoordinates | null): void => useSettingsStore.getState().setSelectedPlot(coordinates);
+export const setSelectedMapUnit = (coordinates?: ICoordinates | null): void => useSettingsStore.getState().setSelectedMapUnit(coordinates);
+
+export const updateSelectedMapUnit = (x: NonNegativeNumber, y: NonNegativeNumber) => {
+  const mapUnits = mapUnitsSelector(useAaStore.getState());
+
+  if (x !== null && y !== null) {
+    const selectedUnits = mapUnits.filter((unit) => {
+      return Number(unit.x) === Number(x) && Number(unit.y) === Number(y);
+    });
+
+    if (selectedUnits.length >= 2) {
+      // house and plot: select house
+      const house = selectedUnits.find((unit) => unit.type === "house");
+
+      if (house) {
+        setSelectedMapUnit(house);
+      } else {
+        throw new Error("It's impossible to have more than one plot in the same place");
+      }
+    } else if (selectedUnits.length === 1) {
+      // select plot or house
+      setSelectedMapUnit(selectedUnits[0]);
+    } else {
+      toast({ title: "No units found" });
+      setSelectedMapUnit(null);
+      console.log("log(init coordinates): No units found");
+    }
+  } else {
+    toast({ title: "Invalid coordinates" });
+    console.log("log(init coordinates): Invalid coordinates");
+  }
+};
 
