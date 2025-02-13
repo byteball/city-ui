3156dfa92bc81fbb708145d080ba85176591cc39
Taump <taump@yandex.ru>
2025-02-13 20:27:14 +0400

    feat: add QRButton component for transaction sharing with QR code functionality

---
+++ src/components/ui/_qr-button.tsx
@@ -0,0 +1,79 @@
+import { QrCode } from "lucide-react";
+import * as QRCode from "qrcode.react";
+import { forwardRef } from "react";
+
+import cn from "classnames";
+import { Button, ButtonProps } from "./button";
+import { Dialog, DialogContent, DialogHeader, DialogTitle, DialogTrigger } from "./dialog";
+import { Tooltip, TooltipContent, TooltipProvider, TooltipTrigger } from "./tooltip";
+
+interface IQRButtonProps extends ButtonProps {
+  href: string;
+}
+
+export const QRButton = forwardRef<HTMLButtonElement, IQRButtonProps>(({ className, children, href, disabled = false, ...props }, ref) => (
+  <div className="flex">
+    <Dialog>
+      <TooltipProvider>
+        <Tooltip>
+          <TooltipTrigger asChild>
+            <DialogTrigger asChild>
+              <Button {...props} disabled={disabled} className="px-3 rounded-tr-none rounded-br-none">
+                <QrCode className="w-4 h-4" />
+              </Button>
+            </DialogTrigger>
+          </TooltipTrigger>
+          <TooltipContent className="max-w-[250px]">
+            <p>Send the transaction from your mobile phone</p>
+          </TooltipContent>
+        </Tooltip>
+      </TooltipProvider>
+
+      <DialogContent className="sm:max-w-[425px]">
+        <DialogHeader>
+          <DialogTitle>
+            Scan this QR code <br /> with your mobile phone
+          </DialogTitle>
+        </DialogHeader>
+        <div className="mx-auto">
+          <a href={href}>
+            <QRCode size={240} className="qr" bgColor="#24292e" fgColor="#fff" renderAs="svg" value={href} />
+          </a>
+        </div>
+
+        <div className="text-xs text-foreground max-w-[220px] mx-auto text-center">
+          Install Obyte wallet for{" "}
+          <a className="text-blue-700" href="#">
+            iOS
+          </a>{" "}
+          or{" "}
+          <a className="text-blue-700" href="#">
+            Android
+          </a>{" "}
+          if you don't have one yet
+        </div>
+      </DialogContent>
+    </Dialog>
+    <TooltipProvider>
+      <Tooltip>
+        <TooltipTrigger asChild>
+          <Button
+            {...props}
+            disabled
+            asChild
+            ref={ref}
+            className={cn("pl-2 rounded-tl-none rounded-bl-none cursor-pointer", {
+              "text-primary-foreground pointer-events-none opacity-50 bg-primary select-none": disabled,
+            })}
+          >
+            <a href={href}>{children}</a>
+          </Button>
+        </TooltipTrigger>
+        <TooltipContent className="max-w-[250px]">
+          <p>This will open your Obyte wallet installed on this computer and send the transaction</p>
+        </TooltipContent>
+      </Tooltip>
+    </TooltipProvider>
+  </div>
+));
+
