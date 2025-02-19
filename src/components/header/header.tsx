040cd898591fea87e3c11eb288f8669f7bc4d95b
Taump <taump@yandex.ru>
2025-02-19 23:40:39 +0400

    feat: update Header component to use NavLink for navigation

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,4 +1,5 @@
 import { FC } from "react";
+import { NavLink } from "react-router";
 
 interface IHeaderProps {}
 
@@ -7,7 +8,7 @@ export const Header: FC<IHeaderProps> = () => {
     <div className="flex-col hidden w-full md:flex">
       <div className="border-b">
         <div className="flex items-center h-16 px-4">
-          <div>City</div>
+          <NavLink to="/">City</NavLink>
         </div>
       </div>
     </div>
