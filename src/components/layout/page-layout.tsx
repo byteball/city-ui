d5132f9bfd6659bf59627b35eeff7997cf6ca0a2
Taump <taump@yandex.ru>
2025-02-20 00:05:28 +0400

    feat: add TextShimmerWave component for enhanced loading indication and integrate with PageLayout in UserPage

--- src/components/layout/page-layout.tsx
+++ src/components/layout/page-layout.tsx
@@ -1,4 +1,5 @@
 import { FC } from "react";
+import { TextShimmerWave } from "../ui/text-shimmer-wave";
 
 interface IPageLayoutProps {
   children: React.ReactNode;
@@ -14,7 +15,13 @@ export const PageLayout: FC<IPageLayoutProps> = ({ children, title, loading = fa
         <h1 className="text-4xl font-extrabold tracking-tight scroll-m-20 lg:text-5xl">{title}</h1>
         {description ? <p className="max-w-4xl mt-4 text-xl text-muted-foreground">{description}</p> : null}
       </div>
-      {loading ? <div>Loading...</div> : <div>{children}</div>}
+      {loading ? (
+        <TextShimmerWave className="font-mono text-md" duration={1.5}>
+          Loading data...
+        </TextShimmerWave>
+      ) : (
+        <div>{children}</div>
+      )}
     </div>
   );
 };
--- src/pages/UserPage/UserPage.tsx
+++ src/pages/UserPage/UserPage.tsx
@@ -1,13 +1,24 @@
 import obyte from "obyte";
 import { Navigate, useParams } from "react-router";
 
+import { PageLayout } from "@/components/layout/page-layout";
+import { useAaStore } from "@/store/aa-store";
+import { useSettingsStore } from "@/store/settings-store";
+
 export default () => {
   const { address } = useParams<{ address: string }>();
+  const inited = useSettingsStore((state) => state.inited);
+  const stateLoaded = useAaStore((state) => state.loaded);
 
   if (!address || !obyte.utils.isValidAddress(address)) {
     return <Navigate to="/not-found" replace />;
   }
+  const loading = !inited || !stateLoaded;
 
-  return <div>UserPage - {address}</div>;
+  return (
+    <PageLayout title="User page" loading={loading}>
+      UserPage - {address}
+    </PageLayout>
+  );
 };
 
