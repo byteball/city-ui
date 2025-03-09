fec09fb9fe26d48778072a9468c16627f1f09f05
Taump <taump@yandex.ru>
2025-03-09 18:59:54 +0100

    fix: refactor RentPlotDialog for improved variable naming and error handling

--- src/components/dialogs/RentPlotDialog.tsx
+++ src/components/dialogs/RentPlotDialog.tsx
@@ -1,9 +1,8 @@
-import cn from "classnames";
-import { FC, KeyboardEvent, useCallback, useEffect, useMemo, useRef, useState } from "react";
+import { FC, KeyboardEvent, useCallback, useMemo, useRef, useState } from "react";
 
 import { generateLink, toLocalString } from "@/lib";
 import { useSettingsStore } from "@/store/settings-store";
-import { Dialog, DialogClose, DialogContent, DialogFooter, DialogHeader, DialogTitle, DialogTrigger } from "../ui/dialog";
+import { Dialog, DialogContent, DialogFooter, DialogHeader, DialogTitle, DialogTrigger } from "../ui/dialog";
 import { Input } from "../ui/input";
 import { Label } from "../ui/label";
 import { QRButton } from "../ui/_qr-button";
@@ -25,30 +24,21 @@ export const RentPlotDialog: FC<IRentPlotDialogProps> = ({ children }) => {
   const putBtnRef = useRef<HTMLButtonElement>(null);
 
   const { symbol, decimals, asset, inited } = useSettingsStore();
-
   const walletAddressFromStore = useSettingsStore((state) => state.walletAddress);
   const selectedMapUnitCoordinates = useSettingsStore((state) => state.selectedMapUnit);
-
   const [selectedMapUnit] = useAaStore((state) => mapUnitsByCoordinatesSelector(state, selectedMapUnitCoordinates as ICoordinates | null));
 
   const handleChange = useCallback(
     (e: React.ChangeEvent<HTMLInputElement>) => {
       const value = e.target.value ?? "";
-
       if (!value || (decimals && getCountOfDecimals(value) <= decimals && Number(value) <= 100000000)) {
-        if (value === "." || value === ",") {
-          setAmount("0.");
-        } else if (!isNaN(Number(value))) {
-          setAmount(value);
-        } else {
-          console.error("Invalid input");
-        }
+        setAmount(value === "." || value === "," ? "0." : !isNaN(Number(value)) ? value : amount);
       }
     },
-    [setAmount, decimals]
+    [setAmount, decimals, amount]
   );
 
-  const handleKeyDawn = useCallback(
+  const handleKeyDown = useCallback(
     (e: KeyboardEvent<HTMLInputElement>) => {
       if (e.key === "Enter") {
         e.preventDefault();
@@ -69,57 +59,37 @@ export const RentPlotDialog: FC<IRentPlotDialogProps> = ({ children }) => {
   const { plot_price, matching_probability, rental_surcharge_factor } = useAaParams();
   const state = useAaStore((state) => state.state);
   const city = state.city_city!;
-
   const countBought = city.total_bought / plot_price;
   const rentedAmount = selectedMapUnit.type === "plot" ? selectedMapUnit?.rented_amount ?? 0 : 0; 
   const amountInSmallestUnit = Number(amount) * 10 ** decimals!;
-
-  const total_working =
-    city.total_land +
-    city.total_rented +
-    amountInSmallestUnit -
-    (selectedMapUnit.type === "plot" ? selectedMapUnit.rented_amount ?? 0 : 0);
-
+  const totalWorking = city.total_land + city.total_rented + amountInSmallestUnit - (selectedMapUnit.type === "plot" ? selectedMapUnit.rented_amount ?? 0 : 0);
   const timestamp = moment.utc().unix();
   const elapsed = timestamp - city.start_ts;
-
   const year = 365 * 24 * 3600;
-
   const countBuysNextYear = (year / elapsed) * countBought;
 
-  // income is overestimated if rental amount is large while $plot.amount is small, then the $plot.amount would be used in full before the year expires
-  const incomeFromOneBuy = (2 * plot_price * matching_probability * amountInSmallestUnit) / total_working;
-
+  const incomeFromOneBuy = (2 * plot_price * matching_probability * amountInSmallestUnit) / totalWorking;
   const yearIncome = incomeFromOneBuy * countBuysNextYear;
-
   const rental_fee = Math.ceil(rental_surcharge_factor * yearIncome);
-
-  let unused_rent = 0;
+  let unusedRent = 0;
 
   if (selectedMapUnit.type === "plot") {
-    const rental_expiry_ts = selectedMapUnit.rental_expiry_ts ?? 0;
-
-    if (timestamp < rental_expiry_ts) {
-      unused_rent = Math.floor((rentedAmount * (rental_expiry_ts - timestamp)) / year);
-
+    const rentalExpiryTs = selectedMapUnit.rental_expiry_ts ?? 0;
+    if (timestamp < rentalExpiryTs) {
+      unusedRent = Math.floor((rentedAmount * (rentalExpiryTs - timestamp)) / year);
       if (amountInSmallestUnit && (amountInSmallestUnit < (selectedMapUnit.rented_amount ?? 0))) {
-        error =
-          "Rental amount cannot be decreased. Min value is " +
-          toLocalString((selectedMapUnit.amount ?? 0) / 10 ** decimals!) +
-          " " +
-          symbol;
+        error =  `Rental amount cannot be decreased. Min value is ${toLocalString((selectedMapUnit.amount ?? 0) / 10 ** decimals!)} ${symbol}`;
       }
     }
   }
 
-  const required_fee = (rental_fee * 1.01 - unused_rent);
-
-  if(required_fee > amountInSmallestUnit) {
+  const requiredFee = (rental_fee * 1.01 - unusedRent);
+  if (requiredFee > amountInSmallestUnit) {
     error = "Not enough paid for rental fee";
   }
   
   const url = generateLink({
-    amount: required_fee,
+    amount: requiredFee,
     data: { rent: 1, plot_num: selectedMapUnit.plot_num, rented_amount: amountInSmallestUnit },
     from_address: walletAddressFromStore!,
     aa: appConfig.AA_ADDRESS,
@@ -137,7 +107,7 @@ export const RentPlotDialog: FC<IRentPlotDialogProps> = ({ children }) => {
         <div className="grid gap-4 py-4">
           <div className="flex flex-col space-y-2">
             <Label htmlFor="amount">Amount</Label>
-            <Input id="amount" error={error} onKeyDown={handleKeyDawn} suffix={symbol} onChange={handleChange} value={amount} />
+            <Input id="amount" error={error} onKeyDown={handleKeyDown} suffix={symbol} onChange={handleChange} value={amount} />
           </div>
         </div>
 
@@ -148,18 +118,20 @@ export const RentPlotDialog: FC<IRentPlotDialogProps> = ({ children }) => {
           <InfoPanel.Item label="Total amount">
             {toLocalString(selectedMapUnit.amount / 10 ** decimals! + Number(amount))} {symbol}
           </InfoPanel.Item>
-          <InfoPanel.Item label="Unused rent">
-            {toLocalString(unused_rent / 10 ** decimals!)} {symbol}
-          </InfoPanel.Item>
+            {unusedRent > 0 && (
+            <InfoPanel.Item label="Previous rental">
+              {toLocalString(unusedRent / 10 ** (decimals || 0))} {symbol}
+            </InfoPanel.Item>
+            )}
           <InfoPanel.Item label="Total fee">
-            {toLocalString(required_fee / 10 ** decimals!)} {symbol}
+            {toLocalString(requiredFee / 10 ** decimals!)} {symbol}
           </InfoPanel.Item>
         </InfoPanel>
 
         <DialogFooter>
           <div className="w-full">
             <QRButton ref={putBtnRef} disabled={!inited || !!error || !amount} className="w-full" href={url}>
-              Send {required_fee > 0 ? toLocalString(required_fee / 10 ** decimals!) : ''} {symbol}
+              Send {requiredFee > 0 ? toLocalString(requiredFee / 10 ** decimals!) : ''} {symbol}
             </QRButton>
             <div className="mt-2 text-gray-400 text-foreground">
               <small>The fee might slightly change. We added 1%, the excess will be returned to you.</small>
@@ -170,4 +142,3 @@ export const RentPlotDialog: FC<IRentPlotDialogProps> = ({ children }) => {
     </Dialog>
   );
 };
-
