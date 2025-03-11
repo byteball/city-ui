f06664856c1734d0aed77b7968e5206e781ab747
Taump <taump@yandex.ru>
2025-03-11 16:43:48 +0100

    feat: enhance UserPlots component with timestamp display and improve UI elements

--- src/pages/UserPage/UserPlots.tsx
+++ src/pages/UserPage/UserPlots.tsx
@@ -1,12 +1,17 @@
+import moment from "moment";
+import { FC, useCallback } from "react";
+import { Link } from "react-router";
+
 import { InfoPanel } from "@/components/ui/_info-panel";
-import { Card, CardContent, CardHeader } from "@/components/ui/card";
-import { toLocalString } from "@/lib";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import { TextScramble } from "@/components/ui/text-scramble";
+
 import { asNonNegativeNumber } from "@/lib/asNonNegativeNumber";
 import { useAaStore } from "@/store/aa-store";
 import { mapUnitsByOwnerAddressSelector } from "@/store/selectors/mapUnitsSelector";
 import { useSettingsStore } from "@/store/settings-store";
-import { FC } from "react";
-import { Link } from "react-router";
+
+import { toLocalString } from "@/lib";
 
 interface IUserPlotsProps {
   address: string;
@@ -14,35 +19,37 @@ interface IUserPlotsProps {
 
 export const UserPlots: FC<IUserPlotsProps> = ({ address }) => {
   const userUnits = useAaStore((state) => mapUnitsByOwnerAddressSelector(state, address));
-  const city = useAaStore((state) => state.state.city_city)!;
-  const { symbol, decimals, asset, inited } = useSettingsStore();
+  const { symbol, decimals } = useSettingsStore();
 
   const userPlots = userUnits.filter((u) => u.type === "plot");
 
-  console.log("userPlots", userPlots);
-
-  const changePlot = ({ x, y }: { x: number; y: number }) => {
+  const changePlot = useCallback(({ x, y }: { x: number; y: number }) => {
     useSettingsStore.getState().setSelectedMapUnit({ x: asNonNegativeNumber(x), y: asNonNegativeNumber(y) });
-  };
+  }, []);
 
   return (
     <div className="mt-8">
       <h2 className="text-xl font-semibold">Plots</h2>
       <div className="grid gap-4 mt-4 md:grid-cols-2 lg:grid-cols-4">
-        {userPlots.map(({ plot_num, x, y, amount, rented_amount = 0 }) => (
+        {userPlots.map(({ plot_num, x, y, amount, rented_amount = 0, ts }) => (
           <Link onClick={() => changePlot({ x, y })} to={`/?c=${x},${y}`} key={plot_num}>
-            <Card key={plot_num}>
-              <CardHeader>
-                <p className="text-sm font-semibold">Plot {plot_num}</p>
+            <Card>
+              <CardHeader className="pb-2 space-y-0 ">
+                <CardTitle>
+                  <TextScramble className="text-sm font-semibold">{`Plot ${plot_num.toString()}`}</TextScramble>
+                </CardTitle>
               </CardHeader>
               <CardContent>
                 <InfoPanel>
                   <InfoPanel.Item label="Total amount">
-                    {toLocalString((amount + rented_amount) / 10 ** decimals!)} <small>{symbol}</small>{" "}
+                    <TextScramble className="inline">{toLocalString((amount + rented_amount) / 10 ** decimals!)}</TextScramble> {" "}
+                    <small><TextScramble className="inline">{symbol!}</TextScramble>{" "}</small>
                   </InfoPanel.Item>
                   <InfoPanel.Item label="Rented">
-                    {toLocalString((rented_amount) / 10 ** decimals!)} <small>{symbol}</small>{" "}
+                  <TextScramble className="inline">{toLocalString((rented_amount) / 10 ** decimals!)}</TextScramble> {" "}
+                    <small><TextScramble className="inline">{symbol!}</TextScramble>{" "}</small>
                   </InfoPanel.Item>
+                  <InfoPanel.Item label="Created at"><TextScramble className="inline">{moment(ts * 1000).format("ll")}</TextScramble></InfoPanel.Item>
                 </InfoPanel>
               </CardContent>
             </Card>
