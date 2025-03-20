e12c77b5b4451ac56fdab25e7df432c09ae6981a
Taump <taump@yandex.ru>
2025-03-20 23:23:26 +0100

    feat: update beautifyParamValue to use percentInputParamNames for percentage parameters

--- src/lib/beautifyParamValue.tsx
+++ src/lib/beautifyParamValue.tsx
@@ -1,15 +1,15 @@
 import { ITokenInfo, paramName } from "@/global";
 import { toLocalString } from "./toLocalString";
 import appConfig from "@/appConfig";
+import { percentInputParamNames } from "./validateParam";
 
-const percentageParams: paramName[] = ["matching_probability", "referral_boost", "p2p_sale_fee", "shortcode_sale_fee", "followup_reward_share"];
 const priceParams: paramName[] = ["plot_price"];
 const addressParams: paramName[] = ["randomness_aa", "mayor"];
 
 export const beautifyParamValue = (name: paramName, value: string | number, tokenInfo: ITokenInfo) => {
   if (priceParams.includes(name)) {
     return `${toLocalString(+value / 10 ** tokenInfo.decimals)} ${tokenInfo.symbol}`;
-  } else if (percentageParams.includes(name)) {
+  } else if (percentInputParamNames.includes(name)) {
     return `${toLocalString(Number(value) * 100)} %`;
   } else if (addressParams.includes(name)) {
     return <a target="_blank" className="text-link" href={`https://${appConfig.TESTNET ? 'testnet' : ''}explorer.obyte.org/address/${value}`} rel="noopener">{value}</a>
