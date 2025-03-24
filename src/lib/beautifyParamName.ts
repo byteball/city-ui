29ad9b1ef9bdba3b99a94218a8b1307363f4cc33
Taump <taump@yandex.ru>
2025-03-24 21:03:18 +0100

    feat: improve beautifyParamName function for better capitalization logic

--- src/lib/beautifyParamName.ts
+++ src/lib/beautifyParamName.ts
@@ -1,5 +1,5 @@
 import { paramName } from "@/global";
 
-export const beautifyParamName = (name: paramName): string  => {
-    return name.split('_').map((word, i) => (i === 0 ? word.charAt(0).toUpperCase() : word.charAt(0)) + word.slice(1)).join(' ');
-}
+export const beautifyParamName = (name: paramName): string => {
+  return name.split("_").map((word, i) => (i !== 0 ? word : word.charAt(0).toUpperCase() + word.slice(1))).join(" ");
+};
