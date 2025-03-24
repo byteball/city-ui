bcf3b9dad6bcee41cb830f47fee08abec56c6211
Taump <taump@yandex.ru>
2025-03-24 20:51:06 +0100

    feat: beautifyParamValue for address

--- src/lib/beautifyParamValue.tsx
+++ src/lib/beautifyParamValue.tsx
@@ -12,7 +12,7 @@ export const beautifyParamValue = (name: paramName, value: string | number, toke
   } else if (percentInputParamNames.includes(name)) {
     return `${toLocalString(Number(value) * 100)} %`;
   } else if (addressParams.includes(name)) {
-    return <a target="_blank" className="text-link" href={`https://${appConfig.TESTNET ? 'testnet' : ''}explorer.obyte.org/address/${value}`} rel="noopener">{value}</a>
+    return <a target="_blank" className="text-link" href={`https://${appConfig.TESTNET ? 'testnet' : ''}explorer.obyte.org/address/${value}`} rel="noopener">{String(value).slice(0, 5)}...{String(value).slice(-5, String(value).length)}</a>
   } else if (name === "attestors") {
     return <>
       {String(value).split(":").map((attestor) => <a target="_blank" key={attestor} className="text-link" href={`https://${appConfig.TESTNET ? 'testnet' : ''}explorer.obyte.org/address/${attestor}`} rel="noopener">{attestor.slice(0, 5)}...{attestor.slice(-5, attestor.length,)}</a>).reduce((prev, curr) => [prev, ", ", curr])}
