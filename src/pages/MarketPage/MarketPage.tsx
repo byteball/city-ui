e7805f1315e12701a76d699ab8219531dd3ca78d
Taump <taump@yandex.ru>
2025-04-12 00:32:13 +0200

    feat: update MarketPage layout with cards for Plots and Shortcodes

--- src/pages/MarketPage/MarketPage.tsx
+++ src/pages/MarketPage/MarketPage.tsx
@@ -1,4 +1,5 @@
 import { PageLayout } from "@/components/layout/page-layout";
+import { Card, CardContent, CardHeader } from "@/components/ui/card";
 
 export default () => {
   return (
@@ -7,7 +8,25 @@ export default () => {
       description="lorem ipsum dolor sit amet consectetur adipisicing elit. Quas cupiditate laboriosam fugiat."
       loading={false}
     >
-      MarketPage
+      <div className="grid grid-cols-4 gap-8">
+        <div className="col-span-3">
+          <Card>
+            <CardHeader>
+              <h2 className="text-xl font-semibold">Plots</h2>
+            </CardHeader>
+            <CardContent>test</CardContent>
+          </Card>
+        </div>
+        <div className="col-span-1">
+          <Card>
+            <CardHeader>
+              <h2 className="text-xl font-semibold">Shortcodes</h2>
+            </CardHeader>
+            <CardContent>test</CardContent>
+          </Card>
+        </div>
+      </div>
     </PageLayout>
   );
 };
+
