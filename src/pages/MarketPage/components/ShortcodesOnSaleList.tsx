452f02a9c51261d44ae72baafd0481b4689e84ed
Taump <taump@yandex.ru>
2025-04-14 21:03:03 +0200

    feat: implement ShortcodesOnSaleList component for displaying and managing shortcode sell orders

---
+++ src/pages/MarketPage/components/ShortcodesOnSaleList.tsx
@@ -0,0 +1,88 @@
+import { FC, useMemo } from "react";
+
+import { QRButton } from "@/components/ui/_qr-button";
+import { Button } from "@/components/ui/button";
+import { ScrollArea } from "@/components/ui/scroll-area";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+
+import { useAaStore } from "@/store/aa-store";
+import { shortcodeSellOrdersSelector } from "@/store/selectors/shortcodeSellOrdersSelector";
+import { useSettingsStore } from "@/store/settings-store";
+
+import { generateLink, toLocalString } from "@/lib";
+
+import appConfig from "@/appConfig";
+
+interface IShortcodesOnSaleListProps {}
+
+export const ShortcodesOnSaleList: FC<IShortcodesOnSaleListProps> = ({}) => {
+  const walletAddress = useSettingsStore((state) => state.walletAddress);
+  const sellOrders = useAaStore(shortcodeSellOrdersSelector);
+  const mySellOrders = useMemo(
+    () => sellOrders.filter((order) => order.owner === walletAddress),
+    [sellOrders, walletAddress]
+  );
+
+  const { decimals, symbol } = useSettingsStore((state) => state);
+
+  const decimalsPow = 10 ** (decimals ?? 0);
+
+  if (sellOrders.length === 0) return <div className="text-muted-foreground">No any shortcodes for sale</div>;
+
+  return (
+    <ScrollArea className="h-full min-h-[400px]" type="always">
+      <Tabs defaultValue="all" className="w-full">
+        <TabsList className="flex justify-between w-full mb-4">
+          <TabsTrigger className="w-[50%]" value="all">
+            All shortcodes
+          </TabsTrigger>
+
+          <TabsTrigger disabled={!walletAddress} className="w-[50%]" value="my">
+            My shortcodes
+          </TabsTrigger>
+        </TabsList>
+
+        <TabsContent value="all">
+          <div>
+            {sellOrders.map(({ name, price }) => (
+              <div key={name} className="flex items-center justify-between p-4 border rounded-md">
+                <div className="font-bold">{name}</div>
+                <Button>
+                  Buy for {toLocalString(price / decimalsPow)} {symbol}
+                </Button>
+              </div>
+            ))}
+          </div>
+        </TabsContent>
+
+        <TabsContent value="my">
+          <div>
+            {mySellOrders.map(({ name, houseNum }) => (
+              <div key={name + houseNum} className="flex items-center justify-between p-4 border rounded-md">
+                <div className="font-bold">{name}</div>
+                <QRButton
+                  href={generateLink({
+                    amount: 10000,
+                    data: {
+                      house_num: houseNum,
+                      edit_house: 1,
+                      sell_shortcode: 1,
+                      shortcode_price: 0,
+                    },
+                    from_address: walletAddress!,
+                    aa: appConfig.AA_ADDRESS,
+                    asset: "base",
+                    is_single: true,
+                  })}
+                >
+                  Withdraw from sale
+                </QRButton>
+              </div>
+            ))}
+          </div>
+        </TabsContent>
+      </Tabs>
+    </ScrollArea>
+  );
+};
+
