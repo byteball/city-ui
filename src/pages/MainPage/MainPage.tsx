82ee9f09f83ee82ca094656e140bec0dd5396c5f
Taump <taump@yandex.ru>
2025-04-19 02:22:02 +0200

    feat: add PrelaunchCard component and integrate PrelaunchForm for pre-launch plot purchases

--- src/pages/MainPage/MainPage.tsx
+++ src/pages/MainPage/MainPage.tsx
@@ -1,4 +1,9 @@
+import moment from "moment";
+
 import { BuyNewPlotCard, GameCard, SelectedUnitMapCard } from "./components";
+import { PrelaunchCard } from "./components/PrelaunchCard";
+
+import appConfig from "@/appConfig";
 
 export default () => (
   <div className="grid gap-6 px-4 md:px-0 md:grid-cols-5">
@@ -8,7 +13,7 @@ export default () => (
     <div className="md:col-span-2">
       <div className="grid grid-cols-1 gap-8">
         <SelectedUnitMapCard sceneType="main" />
-        <BuyNewPlotCard />
+        {moment().isAfter(appConfig.LAUNCH_DATE) ? <BuyNewPlotCard /> : <PrelaunchCard />}
       </div>
     </div>
   </div>
