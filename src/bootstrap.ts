6fc172b9ce4f5460d2277beb26957e5cb02276e2
Taump <taump@yandex.ru>
2025-04-21 16:33:15 +0200

    feat: implement aaRequestHandler for handling AA requests and responses

--- src/bootstrap.ts
+++ src/bootstrap.ts
@@ -1,3 +1,5 @@
+import { aaRequestHandler } from "./events/aaRequestHandler";
+import client from "./services/obyteWsClient";
 import { initializeStore } from "./store/aa-store";
 import { initializeSettings } from "./store/settings-store";
 
@@ -5,6 +7,18 @@ export const bootstrap = async () => {
   await initializeStore();
   await initializeSettings();
 
-  console.log("log: bootstrap done");
+  // @ts-ignore
+  client.subscribe((err, result) => {
+    if (err) return null;
+    const { subject } = result[1];
+
+    if (subject === "light/aa_request") {
+      aaRequestHandler(err, result);
+    } else if (subject === "light/aa_response") {
+      // aaResponseHandler(err, result);
+    }
+
+    console.log("log: bootstrap done");
+  });
 };
 
---
+++ src/events/aaRequestHandler.ts
@@ -0,0 +1,28 @@
+import appConfig from "@/appConfig";
+import { toast } from "@/hooks/use-toast";
+import { useSettingsStore } from "@/store/settings-store";
+
+const showLogs = !!appConfig.TESTNET;
+
+export const aaRequestHandler = (err: string | null, result: any) => {
+  if (showLogs) console.error("log: aaRequestHandler error", err, result);
+  if (err) return null;
+
+  if (showLogs) console.log("log: aaRequestHandler", err, result);
+
+  const { body } = result[1];
+  const { aa_address, unit } = body;
+
+  const { walletAddress } = useSettingsStore.getState();
+  if (!walletAddress) return;
+
+  if (unit && unit.authors && unit.authors.find(({ address }: { address: string }) => address === walletAddress)) {
+    if (showLogs) console.log(`log: user request: ${walletAddress}`, unit);
+
+    toast({
+      title: "New request",
+      description: `Your request has been received and is awaiting confirmation.`,
+    });
+  }
+};
+
