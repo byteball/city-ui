3ebc6f5df0ac3ef1d83354d62068a72db6ca6f1c
Taump <taump@yandex.ru>
2025-04-25 13:13:05 +0200

    feat: init faq

--- src/pages/FaqPage/FaqPage.tsx
+++ src/pages/FaqPage/FaqPage.tsx
@@ -1,4 +1,5 @@
 import { PageLayout } from "@/components/layout/page-layout";
+import { FC, ReactNode } from "react";
 
 const faqs = [
   {
@@ -47,16 +48,27 @@ export default () => {
       description="Have a different question and can’t find the answer you’re looking for? Reach out to our support team by sending us an email and we’ll get back to you as soon as we can."
       loading={false}
     >
-      <div>
-        <dl className="space-y-16 sm:grid sm:grid-cols-2 sm:gap-x-6 sm:gap-y-16 sm:space-y-0 lg:gap-x-10">
-          {faqs.map((faq) => (
-            <div key={faq.id}>
-              <dt className="font-semibold text-white text-base/7">{faq.question}</dt>
-              <dd className="mt-2 text-gray-300 text-base/7">{faq.answer}</dd>
-            </div>
-          ))}
-        </dl>
+      <div className="max-w-5xl prose-xl">
+        <div className="space-y-16 sm:grid sm:gap-x-6 sm:gap-y-16 sm:space-y-0 lg:gap-x-10">
+          <FaqItem>
+            <FaqTitle>What is Obyte City?</FaqTitle>
+            <FaqContent>
+              Obyte City is a community space for Obyte. Here, Obyte community members can establish closer connections
+              with each other, while having their own place in the city and potentially making money in CITY tokens.
+              Anyone can own a plot of land in the City. When buying a plot, it gets random coordinates, and if it
+              happens to be a neighbor of another plot, both owners get:
+              <ul>
+                <li>houses on their plots;</li>
+                <li>
+                  two new plots for each owner at new random locations. The owners can leave the new plots and get CITY
+                  tokens (worth two plots) in exchange, or wait for a new neighbor to appear.
+                </li>
+              </ul>
+            </FaqContent>
+          </FaqItem>
+        </div>
       </div>
+
       <div className="mt-8">
         Any other questions? Ask on{" "}
         <a href="https://discord.obyte.org/" target="_blank" rel="noopener" className="text-blue-400">
@@ -65,5 +77,17 @@ export default () => {
       </div>
     </PageLayout>
   );
+};
+
+const FaqItem: FC<{ children: ReactNode }> = ({ children }) => {
+  return <div>{children}</div>;
+};
+
+const FaqTitle: FC<{ children: ReactNode }> = ({ children }) => {
+  return <div className="font-semibold text-white text-xl/7">{children}</div>;
+};
+
+const FaqContent: FC<{ children: ReactNode }> = ({ children }) => {
+  return <div className="mt-2 text-gray-300 text-base/7">{children}</div>;
 };
 
