a8a5ce31ddf9549f0a0bc72e2a9930ccf6889e7e
Taump <taump@yandex.ru>
2025-05-12 13:42:29 +0200

    feat: enhance AttestationList component to include Discord and Telegram setup links for owners

--- src/pages/UserPage/components/AttestationList.tsx
+++ src/pages/UserPage/components/AttestationList.tsx
@@ -1,36 +1,74 @@
-import { FC } from "react";
+import { FC, useMemo } from "react";
 
 import { IAttestation } from "@/hooks/useAttestations";
 import { getContactUrlByUsername } from "@/lib/getContactUrlByUsername";
 
+import appConfig from "@/appConfig";
+
 interface IAttestationProps {
   data: IAttestation[];
   blockDisplay?: boolean;
+  isOwner?: boolean;
 }
 
-export const AttestationList: FC<IAttestationProps> = ({ data, blockDisplay = false }) => {
+interface IAttestationItemProps extends IAttestation {
+  blockDisplay?: boolean;
+}
+
+export const AttestationList: FC<IAttestationProps> = ({ data, isOwner = false, blockDisplay = false }) => {
   const Wrapper = blockDisplay ? "div" : "span";
+
+  const discordAttestation = useMemo(() => data.find((attestation) => attestation.name === "discord"), [data]);
+  const telegramAttestation = useMemo(() => data.find((attestation) => attestation.name === "telegram"), [data]);
 
   return (
     <Wrapper>
       {data.map((attestation) => {
-        const url = getContactUrlByUsername(attestation.value, attestation.name, attestation.userId);
-
         return (
-          <Wrapper className="mr-4 last:mr-0" key={attestation.name + "-" + attestation.value}>
-            {url ? (
-              <a className="text-link" target="_blank" rel="noopener" href={url}>
-                {attestation.value} ({attestation.name})
-              </a>
-            ) : (
-              <span>
-                {attestation.value} ({attestation.name})
-              </span>
-            )}
-          </Wrapper>
+          <AttestationItem
+            {...attestation}
+            blockDisplay={blockDisplay}
+            key={attestation.name + "-" + attestation.value}
+          />
         );
       })}
+
+      {!discordAttestation && isOwner ? (
+        <Wrapper>
+          discord:{" "}
+          <a className="text-link" href={appConfig.DISCORD_BOT_URL}>
+            set up
+          </a>
+        </Wrapper>
+      ) : null}
+
+      {!telegramAttestation && isOwner ? (
+        <Wrapper>
+          telegram:{" "}
+          <a className="text-link" href={appConfig.TELEGRAM_BOT_URL}>
+            set up
+          </a>
+        </Wrapper>
+      ) : null}
     </Wrapper>
   );
 };
 
+const AttestationItem: FC<IAttestationItemProps> = ({ blockDisplay, name, value, userId }) => {
+  const Wrapper = blockDisplay ? "div" : "span";
+  const url = getContactUrlByUsername(value, name, userId);
+
+  return (
+    <Wrapper className="mr-4 last:mr-0">
+      {url ? (
+        <a className="text-link" target="_blank" rel="noopener" href={url}>
+          {name}: {value}
+        </a>
+      ) : (
+        <span>
+          {name}: {value}
+        </span>
+      )}
+    </Wrapper>
+  );
+};
