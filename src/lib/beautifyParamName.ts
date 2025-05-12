1b4494c3f66024761cd5213fc2f8138a0c9c6623
Taump <taump@yandex.ru>
2025-05-12 18:50:31 +0200

    refactor: enhance beautifyParamName function with specific cases for improved readability

--- src/lib/beautifyParamName.ts
+++ src/lib/beautifyParamName.ts
@@ -1,5 +1,12 @@
 import { paramName } from "@/global";
 
 export const beautifyParamName = (name: paramName): string => {
-  return name.split("_").map((word, i) => (i !== 0 ? word : word.charAt(0).toUpperCase() + word.slice(1))).join(" ");
+  if (name === "p2p_sale_fee") return "P2P sale fee";
+  if (name === "randomness_aa") return "Randomness AA";
+
+  return name
+    .split("_")
+    .map((word, i) => (i !== 0 ? word : word.charAt(0).toUpperCase() + word.slice(1)))
+    .join(" ");
 };
+
