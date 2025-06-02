b71fe72c493c336ae813b05cc615c41b164e0caf
Taump <taump@yandex.ru>
2025-06-02 14:55:21 +0200

    fix: improve house image display size calculation for mayor house

--- src/game/objects/House.ts
+++ src/game/objects/House.ts
@@ -32,7 +32,7 @@ export class House {
     this.houseImage = this.scene.add.image(x, y, this.isMayorHouse ? "mayor-house" : "house");
 
     if (this.isMayorHouse) {
-      this.houseImage.setDisplaySize(this.plotSize, this.plotSize * 1.293);
+      this.houseImage.setDisplaySize(Math.min(this.plotSize, 400), Math.min(this.plotSize * 1.293, 400 * 1.293));
     } else {
       this.houseImage.setDisplaySize(this.plotSize, this.plotSize);
     }
