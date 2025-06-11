27c4ff233fbcfe306341c22b9df3a15a54328fcd
Taump <taump@yandex.ru>
2025-06-11 15:52:36 +0200

    feat: caching attestations

--- src/hooks/useAttestations.ts
+++ src/hooks/useAttestations.ts
@@ -1,6 +1,7 @@
 import appConfig from "@/appConfig";
 import client from "@/services/obyteWsClient";
 import { useAaParams } from "@/store/aa-store";
+import { useCacheStore } from "@/store/cache-store";
 import { useEffect, useState } from "react";
 
 export interface IAttestation {
@@ -19,7 +20,7 @@ const allowedAttestors = Object.entries(appConfig.ATTESTORS);
 export const useAttestations = (address?: string): IAttestationsState => {
   const [attestations, setAttestations] = useState<IAttestationsState>({ data: [], loaded: false });
   const { attestors } = useAaParams();
-
+  const { getUserAttestations, setAttestationsForAddress: saveAttestationsInCache } = useCacheStore()
   const currentCityAttestors = attestors.split(":").map((attestor) => attestor.trim());
 
   useEffect(() => {
@@ -28,37 +29,48 @@ export const useAttestations = (address?: string): IAttestationsState => {
 
       setAttestations({ data: [], loaded: false });
 
-      try {
-        const attestations = await client.api.getAttestations({ address });
-        const userAttestations: IAttestation[] = [];
-        const allowedAddresses = allowedAttestors.map((v) => v[1]); // Extract once
+      const cachedAttestations = getUserAttestations(address);
 
-        attestations.forEach((attestation) => {
-          if (
-            allowedAddresses.includes(attestation.attestor_address) &&
-            currentCityAttestors.includes(attestation.attestor_address)
-          ) {
-            if ("username" in attestation.profile) {
-              const profile = attestation.profile as { username?: string; userId?: string };
-              const username = profile.username;
-              const userId = profile.userId;
-              const resource = allowedAttestors.find((v) => v[1] === attestation.attestor_address)?.[0];
+      if (!cachedAttestations) {
+        try {
+          const attestations = await client.api.getAttestations({ address });
+          if (appConfig.TESTNET) {
+            console.log(`log(useAttestations): Fetched ${attestations.length} attestations for address: ${address}`);
+          }
 
-              if (username && resource) {
-                userAttestations.push({
-                  name: resource,
-                  value: username,
-                  userId: userId ? String(userId) : undefined,
-                });
+          const userAttestations: IAttestation[] = [];
+          const allowedAddresses = allowedAttestors.map((v) => v[1]); // Extract once
+
+          attestations.forEach((attestation) => {
+            if (
+              allowedAddresses.includes(attestation.attestor_address) &&
+              currentCityAttestors.includes(attestation.attestor_address)
+            ) {
+              if ("username" in attestation.profile) {
+                const profile = attestation.profile as { username?: string; userId?: string };
+                const username = profile.username;
+                const userId = profile.userId;
+                const resource = allowedAttestors.find((v) => v[1] === attestation.attestor_address)?.[0];
+
+                if (username && resource) {
+                  userAttestations.push({
+                    name: resource,
+                    value: username,
+                    userId: userId ? String(userId) : undefined,
+                  });
+                }
               }
             }
-          }
-        });
+          });
 
-        setAttestations({ data: userAttestations, loaded: true });
-      } catch (error) {
-        console.error("Error fetching attestations:", error);
-        setAttestations({ data: [], loaded: true });
+          saveAttestationsInCache(address, userAttestations);
+          setAttestations({ data: userAttestations, loaded: true });
+        } catch (error) {
+          console.error("Error fetching attestations:", error);
+          setAttestations({ data: [], loaded: true });
+        }
+      } else {
+        setAttestations({ data: cachedAttestations, loaded: true });
       }
     })();
   }, [address, attestors]);
--- src/store/cache-store.ts
+++ src/store/cache-store.ts
@@ -1,15 +1,71 @@
+import obyte from "obyte";
 import { create, StateCreator } from "zustand";
 import { devtools, persist } from "zustand/middleware";
 
+import appConfig from "@/appConfig";
+import { IAttestation } from '@/hooks/useAttestations';
+
 const LOCAL_STORAGE_KEY = "cache-store";
 const STORAGE_VERSION = 1; // change this to invalidate old persisted data
+const ATTESTATION_CACHE_LIFETIME = 30 * 60 * 1000; // 30 minutes
 
 interface ICacheState {
   inited: boolean;
+  attestations: {
+    [address: string]: {
+      ts: number;
+      attestations: IAttestation[];
+    }
+  };
+  setAttestationsForAddress: (address: string, attestations: IAttestation[]) => void;
+  getUserAttestations: (address: string) => IAttestation[] | null;
 }
 
 const storeCreator: StateCreator<ICacheState> = (set, get) => ({
   inited: false,
+  attestations: {},
+  setAttestationsForAddress: (address, attestations) => {
+    if (obyte.utils.isValidAddress(address) && !Array.isArray(attestations)) {
+      throw new Error("Invalid address or attestations");
+    }
+
+    set((state) => ({
+      attestations: {
+        ...state.attestations,
+        [address]: {
+          ts: Date.now(),
+          attestations
+        }
+      }
+    }));
+  },
+  getUserAttestations: (address) => {
+    if (!address) return null;
+
+    if (!obyte.utils.isValidAddress(address)) {
+      console.error("log(getUserAttestations): Invalid address provided:", address);
+      return null;
+    }
+
+    const entry = get().attestations[address];
+
+    if (!entry && (appConfig.TESTNET)) {
+      console.log(`log(getUserAttestations): No cached attestations found for address: ${address}`);
+      return null;
+    }
+
+    const isExpired = Date.now() - entry.ts >= ATTESTATION_CACHE_LIFETIME;
+
+    if (appConfig.TESTNET) {
+      if (isExpired) {
+        console.log(`log(getUserAttestations): Attestations for address ${address} are expired and will not be returned.`);
+      } else {
+        console.log(`log(getUserAttestations): we use cached attestations for address: ${address}`);
+      }
+    }
+
+    return !isExpired ? entry.attestations : null;
+  }
 });
 
 export const useCacheStore = create<ICacheState>()(
@@ -17,7 +73,7 @@ export const useCacheStore = create<ICacheState>()(
     persist(storeCreator, {
       partialize: (state) => {
         // eslint-disable-next-line @typescript-eslint/no-unused-vars
-        const { ...rest } = state;
+        const { inited, ...rest } = state;
         return rest;
       },
       name: LOCAL_STORAGE_KEY,
@@ -25,13 +81,18 @@ export const useCacheStore = create<ICacheState>()(
       migrate: (persistedState, version) => {
         if (version < STORAGE_VERSION) {
           // Handle migration or reset state
-          return { inited: false };
+          return {
+            inited: false,
+            attestations: {}
+          } as any;
         }
 
         return persistedState as ICacheState;
       },
       onRehydrateStorage: () => (state) => {
-        if (!state) console.error("Failed to rehydrate settings store");
+        if (!state) return console.error("Failed to rehydrate settings store");
+
+        state.inited = true; // Set inited to true after rehydration
       },
     }),
     { name: LOCAL_STORAGE_KEY }
