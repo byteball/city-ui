89b87ac9fbbaa03253f69dbcd5d08d9b52b14e7f
Taump <taump@yandex.ru>
2025-07-04 19:06:12 +0200

    fix: correct wording in NotFound component message for clarity

--- src/pages/ClaimRedirectPage/components/NotFound.tsx
+++ src/pages/ClaimRedirectPage/components/NotFound.tsx
@@ -6,7 +6,7 @@ export const NotFound = () => (
 
     <h1 className="mb-5 text-4xl font-extrabold tracking-tight scroll-m-20 lg:text-5xl">No such plot</h1>
     <div>
-      <small className="text-muted-foreground">It may already have become a neighbor to someone else</small>
+      <small className="text-muted-foreground">It may have already become a neighbor to someone else</small>
     </div>
   </div>
 );
