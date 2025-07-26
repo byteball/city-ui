b90caba92d6180e323ac3e6f23dd8d4725ce63d9
tony <tonyofbyteball@users.noreply.github.com>
2025-07-27 00:59:46 +0200

    no lowercasing of links

--- src/lib/getSocialUrl.ts
+++ src/lib/getSocialUrl.ts
@@ -1,52 +1,52 @@
 export const getSocialUrl = (social: string, value: string): string | null => {
   let socialLower = social.toLowerCase().trim();
-  let valueLower = value.toLowerCase().trim();
+  let trimmedValue = value.trim();
 
-  if (valueLower.startsWith("@")) {
-    valueLower = valueLower.slice(1); // Remove leading '@' if present
+  if (trimmedValue.startsWith("@")) {
+    trimmedValue = trimmedValue.slice(1); // Remove leading '@' if present
   }
 
   switch (socialLower) {
     case "twitter":
-      return `https://x.com/${valueLower}`;
+      return `https://x.com/${trimmedValue}`;
     case "x":
-      return `https://x.com/${valueLower}`;
+      return `https://x.com/${trimmedValue}`;
     case "github":
-      return `https://github.com/${valueLower}`;
+      return `https://github.com/${trimmedValue}`;
     case "telegram":
-      return `https://t.me/${valueLower}`;
+      return `https://t.me/${trimmedValue}`;
     case "youtube":
-      return `https://www.youtube.com/@${valueLower}`;
+      return `https://www.youtube.com/@${trimmedValue}`;
     case "twitch":
-      return `https://www.twitch.tv/${valueLower}`;
+      return `https://www.twitch.tv/${trimmedValue}`;
     case "reddit":
-      return `https://www.reddit.com/user/${valueLower}`;
+      return `https://www.reddit.com/user/${trimmedValue}`;
     case "linkedin":
-      return `https://www.linkedin.com/in/${valueLower}`;
+      return `https://www.linkedin.com/in/${trimmedValue}`;
     case "facebook":
-      return `https://www.facebook.com/${valueLower}`;
+      return `https://www.facebook.com/${trimmedValue}`;
     case "instagram":
-      return `https://www.instagram.com/${valueLower}`;
+      return `https://www.instagram.com/${trimmedValue}`;
     case "medium":
-      return `https://medium.com/@${valueLower}`;
+      return `https://medium.com/@${trimmedValue}`;
     case "mastodon":
-      return `https://mastodon.social/@${valueLower}`;
+      return `https://mastodon.social/@${trimmedValue}`;
     case "bluesky":
-      return `https://bsky.app/profile/${valueLower}`;
+      return `https://bsky.app/profile/${trimmedValue}`;
     case "threads":
-      return `https://www.threads.net/@${valueLower}`;
+      return `https://www.threads.net/@${trimmedValue}`;
     case "snapchat":
-      return `https://www.snapchat.com/add/${valueLower}`;
+      return `https://www.snapchat.com/add/${trimmedValue}`;
     case "tiktok":
-      return `https://www.tiktok.com/@${valueLower}`;
+      return `https://www.tiktok.com/@${trimmedValue}`;
     case "patreon":
-      return `https://www.patreon.com/cw/${valueLower}`;
+      return `https://www.patreon.com/cw/${trimmedValue}`;
     case "buymeacoffee":
-      return `https://www.buymeacoffee.com/${valueLower}`;
+      return `https://www.buymeacoffee.com/${trimmedValue}`;
     case "website":
     case "blog":
-      return (valueLower.startsWith("http://") || valueLower.startsWith("https://")) ? valueLower : `https://${valueLower}`;
+      return (trimmedValue.startsWith("http://") || trimmedValue.startsWith("https://")) ? trimmedValue : `https://${trimmedValue}`;
     default:
-      return (valueLower.startsWith("http://") || valueLower.startsWith("https://")) ? valueLower : null;
+      return (trimmedValue.startsWith("http://") || trimmedValue.startsWith("https://")) ? trimmedValue : null;
   }
 };
