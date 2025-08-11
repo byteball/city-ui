29572b82f97d692796bb5b976d80b1021d8126ed
Taump <taump@yandex.ru>
2025-08-11 20:31:57 +0200

    feat: add rewards balance display and buy option in `UserStats` component

--- src/pages/UserPage/components/UserStats.tsx
+++ src/pages/UserPage/components/UserStats.tsx
@@ -1,15 +1,23 @@
+import { InfoIcon } from "lucide-react";
 import { FC, memo, useEffect, useState } from "react";
 
+import { QRButton } from "@/components/ui/_qr-button";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { Skeleton } from "@/components/ui/skeleton";
-import { toLocalString } from "@/lib";
+import { Tooltip, TooltipContent, TooltipProvider, TooltipTrigger } from "@/components/ui/tooltip";
+
+import { getPlotPrice } from "@/aaLogic/getPlotPrice";
+import { generateLink, toLocalString } from "@/lib";
 import { calculateUserProbability } from "@/lib/calculateUserProbability";
+
 import { useAaParams, useAaStore } from "@/store/aa-store";
 import { mapUnitsByOwnerAddressSelector } from "@/store/selectors/mapUnitsSelector";
 import { useSettingsStore } from "@/store/settings-store";
 
 import httpClient from "@/services/obyteHttpClient";
 import client from "@/services/obyteWsClient";
+
+import appConfig from "@/appConfig";
 
 interface IUserStatsProps {
   address: string;
@@ -23,14 +31,19 @@ interface IWalletBalance {
 
 export const UserStats: FC<IUserStatsProps> = memo(({ address }) => {
   const [walletBalance, setWalletBalance] = useState<IWalletBalance>({ amount: 0, loaded: false, loading: false });
-  const { decimals, symbol, asset } = useSettingsStore((state) => state);
+  const { decimals, symbol, asset, walletAddress } = useSettingsStore((state) => state);
   const balance = useAaStore((state) => Number(state.state?.[`user_land_${address}`] ?? 0));
+  const followUpBalance = useAaStore((state) => Number(state.state?.[`balance_${address}`] ?? 0));
   const city = useAaStore((state) => state.state.city_city)!;
+
+  const params = useAaParams();
+  const { totalPrice } = getPlotPrice(params);
 
   const balanceView = toLocalString(balance / 10 ** (decimals ?? 0));
   const walletBalanceView = toLocalString(walletBalance.amount / 10 ** (decimals ?? 0));
   const userUnits = useAaStore((state) => mapUnitsByOwnerAddressSelector(state, address));
-  const { matching_probability, referral_boost } = useAaParams();
+  const { matching_probability, referral_boost } = params;
+
   const overallProb = calculateUserProbability(
     userUnits.filter((unit) => unit.type === "plot"),
     city,
@@ -68,6 +81,19 @@ export const UserStats: FC<IUserStatsProps> = memo(({ address }) => {
     };
   }, [address, asset]);
 
+  const canBuyFromBalance = totalPrice <= followUpBalance && address === walletAddress;
+
+  const buyFromBalanceUrl = generateLink({
+    amount: 1e4,
+    aa: appConfig.AA_ADDRESS,
+    from_address: address,
+    is_single: true,
+    data: {
+      buy_from_balance: 1,
+      buy: 1
+    }
+  });
+
   return (
     <div className="grid gap-4 mt-8 md:grid-cols-2 lg:grid-cols-4 auto-rows-auto">
       {walletBalance.loaded ? (
@@ -100,7 +126,35 @@ export const UserStats: FC<IUserStatsProps> = memo(({ address }) => {
           </div>
         </CardContent>
       </Card>
-    </div>
+      <Card>
+        <CardHeader className="flex flex-row items-center justify-between pb-2 space-y-0">
+          <CardTitle className="flex space-x-1 text-sm font-medium">
+            <div>Rewards balance</div>
+            <TooltipProvider>
+              <Tooltip>
+                <TooltipTrigger className="cursor-pointer">
+                  <InfoIcon className="w-3 h-3 ml-1 text-muted-foreground" />
+                </TooltipTrigger>
+                <TooltipContent className="w-auto max-w-[200px]">
+                  <div className="text-sm">Accumulated from follow-up rewards</div>
+                </TooltipContent>
+              </Tooltip>
+            </TooltipProvider>
+          </CardTitle>
+        </CardHeader>
+        <CardContent>
+          <div className="text-2xl font-bold">
+            <div>{toLocalString((followUpBalance / 10 ** decimals!).toFixed(4))} {symbol}</div>
+          </div>
+
+          {<div className="mt-2">
+            <QRButton href={buyFromBalanceUrl} disabled={!canBuyFromBalance} size="sm" variant="secondary">
+              Buy a plot from this balance
+            </QRButton>
+          </div>}
+        </CardContent>
+      </Card>
+    </div >
   );
 });
 
